d195a9b221f527a1173bfa09e515e6d0b55f9154
Serhii Varakuta <flood.code@gmail.com>
2022-11-21 12:55:32 +0200

    Allow owner to add tokens and set fees

--- contracts/bridge/TokenManager.sol
+++ contracts/bridge/TokenManager.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 import "./SignerOwnable.sol";
 import "./ERC20Bridgeable.sol";
 
@@ -11,18 +12,18 @@ enum TokenType {
     MINTED
 }
 
-contract TokenManager is Initializable, SignerOwnable {
+contract TokenManager is Initializable, Ownable, SignerOwnable {
     mapping(address => TokenType) public supportedTokens;
 
     function initialize(address _signerStorage) external initializer {
         _setSignerStorage(_signerStorage);
     }
 
-    function setToken(address _token, TokenType _tokenType) external onlySigner {
+    function setToken(address _token, TokenType _tokenType) external onlyOwner {
         _setToken(_token, _tokenType);
     }
 
-    function deployToken(string memory name, string memory symbol) external onlySigner returns (address) {
+    function deployToken(string memory name, string memory symbol) external onlyOwner returns (address) {
         ERC20Bridgeable erc20Bridgeable = new ERC20Bridgeable(name, symbol);
         _setToken(address(erc20Bridgeable), TokenType.MINTED);
 
